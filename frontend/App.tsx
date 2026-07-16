96b0bab1ddbe71c82fdb48f676d1d475aa55c017
AlyssaDambeck <al667195@ucf.edu>
2026-07-15 23:00:27 -0400

    fixed structuring

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,9 +1,22 @@
-import './App.css';
-import LoginPage from './pages/LoginPage.tsx';
-function App()
-{
-return (
-<LoginPage />
-);
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch(() => setMessage("Backend not running"));
+  }, []);
+
+  return (
+    <div>
+      <h1>PotBuddy</h1>
+
+      <h2>{message}</h2>
+    </div>
+  );
 }
+
 export default App;
