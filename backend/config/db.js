8fb13f4f7c5ad275d29b3dc3a4d811668d566d1c
BeanTechCreates <vcharles1111@gmail.com>
2026-07-23 07:25:38 -0400

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,8 @@
+const dns = require("node:dns");
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const uri = process.env.MONGODB_URI;
 
