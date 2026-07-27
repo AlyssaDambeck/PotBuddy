fb37e2b0e63248b9d6f154c39d1c3bdf75eb7288
AlyssaDambeck <al667195@ucf.edu>
2026-07-27 02:40:20 -0400

    fixing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,12 @@
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 
-import "./index.css";
 import App from "./App.tsx";
+import "./index.css";
 
 const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || "";
+console.log(import.meta.env.VITE_GOOGLE_CLIENT_ID);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
