8a7dadd373f470611bbc4568af2f88ed3c8c3ed4
AlyssaDambeck <al667195@ucf.edu>
2026-07-29 01:30:36 -0400

    removed helmet

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const helmet = require('helmet');
+
 
 const authRoutes = require("./routes/authRoutes");
 const plantRoutes = require("./routes/plantRoutes");
